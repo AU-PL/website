04cff711d385ee5ec44ce6bac7885c40625966b0
Harley D. Eades III <harley.eades@gmail.com>
2025-08-27 12:20:17 -0400

    Added homework 1.

---
+++ hwk/1/bin/main.ml
@@ -0,0 +1,9 @@
+(* 1. Define a variable using a let-expression called 'favMove' and set it to
+      your favorite movie as a string (string's in OCaml are written using
+      double-quotes.) *)
+
+
+(* 2. Define a function, called maxi, that takes in two integer parameters and
+       returns the max between them.  *)
+
+
