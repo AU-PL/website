f938a4c374aa9f1fe4f9b598963a4acd09f4630b
Harley D. Eades III <harley.eades@gmail.com>
2025-08-27 12:37:16 -0400

    Added homework 1.

--- hwk/1/bin/main.ml
+++ hwk/1/bin/main.ml
@@ -6,4 +6,13 @@
 (* 2. Define a function, called maxi, that takes in two integer parameters and
        returns the max between them.  *)
 
+(* 3. Use `dune utop` to run the maxi function on three pairs of inputs. Copy
+      and paste your **complete** transcript here; this should include getting
+      utop to import this files contents.  *)
 
+(* Copy your transcript below this line, but inside the comment:
+ *
+ * 
+ *)
+
+ 
